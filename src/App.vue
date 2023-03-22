f2571500513e515f3fd96ff7dfc81ec81dd45c8a
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2023-03-22 20:01:31 +0100

    Added Components Navbar and AuthButtons

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
+import AuthButtons from './components/AuthButtons.vue';
 
 </script>
 
 <template>
   <header>
     <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
+      <NavBar></NavBar>
+      <AuthButtons></AuthButtons>
     </div>
   </header>
   
---
+++ src/components/AuthButtons.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="AuthButtons">
+      <button class="login" @click="login">Login</button>
+      <button class="signup" @click="signup">Sign Up</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      login() {
+        // code to handle login button click
+      },
+      signup() {
+        // code to handle signup button click
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .AuthButtons {
+    display: flex;
+    align-items: center;
+  }
+  
+  .AuthButtons button {
+    margin-left: 20px;
+    padding: 8px 16px;
+    border: none;
+    background-color: #fff;
+    color: #333;
+    font-size: 14px;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  .AuthButtons button:hover {
+    background-color: #f5f5f5;
+  }
+  </style>
+  
