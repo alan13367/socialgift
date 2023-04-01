9da128feb10f9ec8a914a1d8f330c1aa2068d8b6
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2023-04-01 18:37:33 +0200

    Added SignUp View

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -3,8 +3,11 @@ import header from "@/components/Header.vue";
 export default {
   data() {
     return {
+      name: '',
+      lastName: '',
       email: '',
       password: '',
+      imageUrl: '',
     };
   },
   methods: {
@@ -18,15 +21,23 @@ export default {
 <header>
 </header>
 <template>
-    <h2>Login</h2>
-    <div class="login-container">
-      <form class="login-form" @submit.prevent="submitForm">
-        
+    <h2>Sign Up</h2>
+    <div class="signup-container">
+      <form class="signup-form" @submit.prevent="submitForm">
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="name" required>
+  
+        <label for="last-name">Last Name:</label>
+        <input type="text" id="last-name" v-model="lastName" required>
+  
         <label for="email">Email:</label>
         <input type="email" id="email" v-model="email" required>
   
         <label for="password">Password:</label>
         <input type="password" id="password" v-model="password" required>
+  
+        <label for="image-url">Image URL:</label>
+        <input type="url" id="image-url" v-model="imageUrl" required>
   
         <button type="submit">Submit</button>
       </form>
@@ -36,14 +47,14 @@ export default {
 
   
   <style>
-  .login-container {
+  .signup-container {
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
   }
   
-  .login-form {
+  .signup-form {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -55,5 +66,4 @@ export default {
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
   }
   </style>
-  
   
