8ba71ec771143725cd294ca0285aaaa7d61f48de
masolferino <124627879+masolferino@users.noreply.github.com>
2023-04-01 19:06:23 +0200

    Modificado Header SingIn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <Header />
-    <main>
-      <img src="@/assets/logo.png" alt="Vue logo" />
-      <HelloWorld msg="Welcome to Your Vue.js App" />
-    </main>
-    <Footer />
+
+  <Header />
+  <main>
+  <img src="background.png" alt="Imagen de fondo" class="background">
+  <div class="container">
+    <div class="left-container">
+      <h2 class="title">La mejor manera de gestionar tus regalos</h2>
+      <h3 class="subtitle">Prioriza y crea un presupuesto para cada ocasión. Comunícate con tus amigos para no repetir.</h3>
+      <div class="button-container">
+        <button class="start-button">Empezar</button>
+        <a href="#" class="video-link"><i class="fas fa-play"></i> Video introducción</a>
+      </div>
+    </div>
+    <div class="right-container">
+      <img src="mobile.png" alt="Imagen de la aplicación en un dispositivo móvil">
+    </div>
   </div>
+  </main>
+  <Footer />
 </template>
+
 
 <script>
 import Header from "@/components/HeaderLogIn.vue";
@@ -24,5 +36,76 @@ export default {
 
 <style scoped>
 
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
+.left-container {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  font-family: 'Inter', sans-serif;
+  font-size: 72;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 30px;
+  max-width: 100%;
+}
+
+.subtitle {
+  font-family: 'Inter', sans-serif;
+  font-size: 28;
+  font-weight: 400;
+  text-align: center;
+  margin-bottom: 50px;
+  max-width: 100%;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.start-button {
+  font-family: 'Inter', sans-serif;
+  font-size: 24;
+  font-weight: 600;
+  text-align: center;
+  padding: 12 40;
+  border-radius: 25;
+  background-color: #0080ff;
+  color: #fff;
+  margin-bottom: 20px;
+}
+
+.video-link {
+  font-family: 'Inter', sans-serif;
+  font-size: 24;
+  font-weight: 600;
+  text-align: center;
+  color: #0080ff;
+}
+
+.video-link i {
+  margin-right: 10;
+}
+
+.right-container {
+  width: 50%;
+}
+
+.right-container img {
+  width: 100%;
+  height: auto;
+}
 </style>
