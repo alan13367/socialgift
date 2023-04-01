6d9f7b77368ad8df6b1455413656f0d08c6b37a1
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2023-04-01 20:32:38 +0200

    Added a Component for FriendList and a ChatsView

---
+++ src/components/FriendList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h2>My Chats</h2>
+      <ul>
+        <li v-for="friend in friends" :key="friend.id" @click="selectFriend(friend)">
+          {{ friend.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FriendList',
+    data() {
+      return {
+        friends: [
+          { id: 1, name: 'Alice' },
+          { id: 2, name: 'Bob' },
+          { id: 3, name: 'Charlie' },
+        ],
+      };
+    },
+    methods: {
+      selectFriend(friend) {
+        this.$emit('select-friend', friend);
+      },
+    },
+  };
+  </script>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import ManageFriends from '../views/ManageFriendsView.vue'
 import FriendsPending from '../views/FriendsPendingView.vue'
 import UserProfile from '../views/UserProfileView.vue'
 import SignUpView from '../views/SignUpView.vue'
+import ChatsView from '../views/ChatsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -52,11 +53,15 @@ const router = createRouter({
 
     },
     {
+      path: '/Chats',
+      name:'Chats',
+      component: ChatsView
+    },
+    {
       path: '/UserProfile',
       name: 'UserProfile',
       component: UserProfile
     }
-    
   ]
 })
 
