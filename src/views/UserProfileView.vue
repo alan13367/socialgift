5fc1dc86e859996cbc7e12b4bf61cc59792d4b83
masolferino <124627879+masolferino@users.noreply.github.com>
2023-04-02 20:02:53 +0200

    cambiosprofile

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -3,21 +3,22 @@
     <h2>Perfil</h2>
     <div>
       <div>
-        <img :src="userImageUrl" alt="Foto de perfil" />
-        <h3>{{ userFullName }}</h3>
+        <img src="@/assets/Imagenes/userimage.png" alt="Foto de perfil" />
+        <h3> Nombre Usuario Completo</h3>
       </div>
       <div>
         <button>Send Friend request</button>
         <div class="wishlist">
           <div v-for="item in wishlistItems" :key="item.id">
             <h4>WishList</h4>
-          </div>
+          </div> 
         </div>
       </div>
     </div>
   </div>
 </template>
 
+<sty
 
 
 
