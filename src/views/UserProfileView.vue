175ae6dc8675085a2cb80c08700db76259531d80
masolferino <124627879+masolferino@users.noreply.github.com>
2023-04-02 18:18:43 +0200

    *

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -19,17 +19,6 @@
 </template>
 
 
-  <script>
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
-
-export default {
-  components: {
-    Header,
-    Footer,
-  },
-}
-</script>
 
 
   
