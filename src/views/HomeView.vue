6a43eb0e737911af846dbf6de3aff08b620027a2
masolferino <124627879+masolferino@users.noreply.github.com>
2023-04-02 14:11:41 +0200

    arreglo homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,6 +20,11 @@
 </template>
 
 <style scoped>
+
+main {
+  max-width: 100%;
+}
+
 .background {
   position: fixed;
   top: 0;
@@ -37,11 +42,12 @@
 
 .container {
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
-  height: 100vh;
-  position: relative;
+  height: 80%;
+  width: 80%;
+  margin-left: 10%;
 }
 
 .left-container {
@@ -49,68 +55,68 @@
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
 }
 
 .title {
   font-family: 'Inter', sans-serif;
-  font-size: 72px;
+  font-size: 72;
   font-weight: 600;
-  text-align: center;
-  margin-bottom: 30px;
+  text-align: left;
+  margin-bottom: 30;
   max-width: 100%;
 }
 
 .subtitle {
   font-family: 'Inter', sans-serif;
-  font-size: 28px;
+  font-size: 28;
   font-weight: 400;
-  text-align: center;
-  margin-bottom: 50px;
-  max-width: 100%;
+  text-align: left;
+  margin-bottom: 50;
+  max-width: 60%;
 }
 
 .button-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: left;
 }
 
 .start-button {
   font-family: 'Inter', sans-serif;
-  font-size: 24px;
+  font-size: 24;
   font-weight: 600;
-  text-align: center;
-  padding: 12px 40px;
-  border-radius: 25px;
+  text-align: left;
+  padding: 12px 40;
+  border-radius: 25;
   background-color: #0080ff;
   color: #fff;
-  margin-bottom: 20px;
+  margin-bottom: 20;
 }
 
 .video-link {
   font-family: 'Inter', sans-serif;
-  font-size: 24px;
+  font-size: 24;
   font-weight: 600;
-  text-align: center;
+  text-align: left;
   color: #0080ff;
 }
 
 .video-link i {
-  margin-right: 10px;
+  margin-right: 10;
 }
 
 .right-container {
   width: 50%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 32;
 }
 
 .right-container img {
-  width: 100%;
-  height: 100%;
+  max-width: 90%;
+  max-height: 90%;
 }
 </style>
