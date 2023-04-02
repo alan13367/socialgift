a0e274c94b0c990f00851c237203406b697566bd
masolferino <124627879+masolferino@users.noreply.github.com>
2023-04-02 22:53:35 +0200

    Se agregaron dos listas en FriendsPending

--- src/views/FriendsPendingView.vue
+++ src/views/FriendsPendingView.vue
@@ -1,125 +1,105 @@
 <template>
   <main>
-  <div class="search-bar">
-    <input type="text" v-model="searchQuery" placeholder="Search for gifts...">
-    <button @click="search">Search</button>
-  </div>
-  <div class="container">
-    <div class="left-container">
-      <h3>Solicitudes pendientes</h3>
-      <ul>
-        <li v-for="request in pendingRequests" :key="request.id">
-          <button @click="acceptRequest(request)">Aceptar</button>
-          <span>{{ request.name }}</span>
-          <img :src="request.image" alt="Request image">
-        </li>
-      </ul>
+    <div class="search-bar">
+      <input type="text" v-model="searchQuery" placeholder="Search for gifts...">
+      <button @click="search">Search</button>
     </div>
-    <div class="right-container">
-      <h3>Envíos pendientes</h3>
-      <ul>
-        <li v-for="request in pendingSends" :key="request.id">
-          <button @click="markAsPending(request)">Pendiente</button>
-          <span>{{ request.name }}</span>
-        </li>
-      </ul>
-    </div>
-  </div> 
+    <div class="container">
+      <div class="left-container">
+        <h3>Solicitudes pendientes</h3>
+        <div v-for="item in leftItems" :key="item.id" class="item">
+          <button>Aceptar</button>
+          <img class="imgfriends" src="@/assets/Imagenes/friends.png" alt="friends">
+          <p>{{ item.name }}</p>
+        </div>
+      </div>
+      <div class="right-container">
+        <h3>Envíos pendientes</h3>
+        <div v-for="item in rightItems" :key="item.id" class="item">
+          <button>Pendiente</button>
+          <p>{{ item.name }}</p>
+        </div>
+      </div>
+    </div> 
   </main>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        request: [
-          {
-            id: 1,
-            name: 'Friends1',
-            image: 'src/assets/Imagenes/gift.jpg',
-          },
-          {
-            id: 2,
-            name: 'Friends2',
-            image: 'src/assets/Imagenes/gift.jpg',
-          },
-          {
-            id: 3,
-            name: 'Friends3',
-            image: 'src/assets/Imagenes/gift.jpg',
-          },
-        ],
-        searchQuery: '',
-      }
-    },
-    methods: {
-      acceptRequest(request) {
-        this.$store.dispatch('acceptRequest', request)
-      },
-      markAsPending(request) {
-        this.$store.dispatch('markAsPending', request)
-      },
-      search() {
-        this.$store.dispatch('search', this.searchQuery)
-      },
-    },
-    computed: {
-      filteredGifts() {
-        return this.gifts.filter(gift => gift.name.toLowerCase().includes(this.searchQuery.toLowerCase()))
-      },
-    },
-  }
-  </script>
-
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      leftItems: [
+        { id: 1, name: 'Nombre Completo 1' },
+        { id: 2, name: 'Nombre Completo 2' },
+        { id: 3, name: 'Nombre Completo 3' },
+        { id: 4, name: 'Nombre Completo 4' },
+      ],
+      rightItems: [
+        { id: 1, name: 'Nombre Completo 5' },
+        { id: 2, name: 'Nombre Completo 6' },
+        { id: 3, name: 'Nombre Completo 7' },
+        { id: 4, name: 'Nombre Completo 8' },
+      ],
+    }
+  },
+  
+}
+</script>
 
 <style scoped>
-main {
-  width: 100%;
-  height: 100%;
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 80%;
+  width: 80%;
+  margin-left: 10%;
+  margin-top: 5%;
+  position: relative;
+  z-index: 1;
+}
+
+.left-container {
+  width: 40%;
+  height: 60%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
 }
- 
 
-  .container {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    height: 80%;
-    width: 80%;
-    margin-left: 10%;
-    margin-top: 5%;
-    position: relative;
-    z-index: 1;
-  }
+.right-container {
+  width: 40%;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-left: 32px;
+}
 
-  .left-container {
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-  }
+.item {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
 
-  h3 {
-    font-family: 'Inter', sans-serif;
-    font-size: 24px;
-    font-weight: 600;
-    text-align: left;
-    margin-bottom: 30px;
-    max-width: 100%;
-  }
+img.imgfriends {
+  width: 30px;
+  height: 30px;
+  margin-left: 10px;
+}
 
-  .right-container {
-    width: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-left: 32px;
-  }
+h3 {
+  font-family: 'Inter', sans-serif;
+  font-size: 24px;
+  font-weight: 600;
+  text-align: left;
+  margin-bottom: 30px;
+  max-width: 100%;
+}
 
   .search-bar {
     position: relative;
@@ -157,6 +137,16 @@ main {
   button:hover {
     background-color: #5b2eab;
   }
-</style>
 
- 
+  .item {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  .item p {
+    margin: 0;
+    margin-left: 10px;
+  }
+</style>
