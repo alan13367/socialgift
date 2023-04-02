d229fbf7a2589e6a62ea0d0c07b980b243866253
masolferino <124627879+masolferino@users.noreply.github.com>
2023-04-02 20:18:53 +0200

    borrado de background en friendspending

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -1,26 +1,78 @@
 <template>
+  <main>
   <div>
     <h2>Perfil</h2>
-    <div>
-      <div>
-        <img src="@/assets/Imagenes/userimage.png" alt="Foto de perfil" />
-        <h3> Nombre Usuario Completo</h3>
+    <div class="user-info">
+      <div class="user-details">
+        <img src="@/assets/Imagenes/userimage.png" alt="Foto de perfil" class="user-image" />
+        <h3 class="user-name">Nombre Usuario Completo</h3>
       </div>
-      <div>
+      <div class="user-actions">
         <button>Send Friend request</button>
         <div class="wishlist">
-          <div v-for="item in wishlistItems" :key="item.id">
-            <h4>WishList</h4>
-          </div> 
+          <h4>WishList</h4>
+          <div class="wishlist-buttons">
+            <button v-for="item in wishlistItems" :key="item.id">{{ item.name }}</button>
+          </div>
         </div>
       </div>
     </div>
   </div>
+  </main>
 </template>
 
 <style>
+main {
+  width: 100%;
+  height: 100%;
+}
 
+.user-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.user-details {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  height: 50%;
+  width: 90%;
+  max-height: 50vh;
+}
+
+.user-image {
+  height: 90%;
+  margin-right: 1rem;
+}
+
+.user-name {
+  max-height: 50%;
+}
+
+.user-actions {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  height: 50%;
+}
+
+.wishlist {
+  display: flex;
+  flex-direction: column;
+}
+
+.wishlist-buttons {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
 </style>
+
 
 
 
