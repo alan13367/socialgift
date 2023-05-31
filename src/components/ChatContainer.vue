e461456307edde4bb6c59694ea7702ece6d01824
Alan Beltran Pozo <alan.beltran@students.salle.url.edu>
2023-05-31 02:38:09 +0200

    Big Quality of Life Changes to Chat

--- src/components/ChatContainer.vue
+++ src/components/ChatContainer.vue
@@ -1,4 +1,24 @@
+<template>
+    <div class="chat-container">
+      <div class="chat-messages">
+        <div class="message" v-for="message in messages" :class="{'outgoing': message.outgoing, 'incoming': !message.outgoing}">
+          <div class="message-bubble">
+            <p>{{ message.text }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="chat-input">
+        <input type="text" v-model="newMessageText" placeholder="Type your message here..." />
+        <button @click="sendMessage()">Send</button>
+      </div>
+    </div>
+  </template>
+
+
+
 <script>
+import { registerRuntimeHelpers } from '@vue/compiler-core';
+
 export default {
   name: 'ChatContainer',
   props: {},
@@ -52,6 +72,7 @@ export default {
                 text: message.content,
                 outgoing: message.user_id_send !== this.selectedFriendId 
                 }));
+                this.messages.reverse();
           } else {
             console.error('Error: Messages data is missing');
             console.error(response);
@@ -83,18 +104,72 @@ export default {
 </script>
 
 
-<template>
-  <div class="chat-container">
-    <div class="chat-messages">
-      <div class="message" v-for="message in messages" :class="{'outgoing': message.outgoing, 'incoming': !message.outgoing}">
-        <div class="message-bubble">
-          <p>{{ message.text }}</p>
-        </div>
-      </div>
-    </div>
-    <div class="chat-input">
-      <input type="text" v-model="newMessageText" placeholder="Type your message here..." />
-      <button @click="sendMessage()">Send</button>
-    </div>
-  </div>
-</template>
+<style scoped>
+.chat-container {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+}
+
+.chat-messages {
+  flex: 1;
+  overflow-y: auto;
+  padding: 10px;
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+.message {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 10px;
+  flex-direction: row-reverse;
+}
+
+.message-bubble {
+  max-width: 70%;
+  padding: 10px;
+  border-radius: 10px;
+  color: #fff;
+}
+
+.outgoing .message-bubble {
+  background-color: #A33DA5;
+  margin-left: auto;
+}
+
+.incoming .message-bubble {
+  background-color: #E94AEC;
+  margin-right: auto;
+}
+
+.chat-input {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: #f0f0f0;
+}
+
+.chat-input input {
+  flex: 1;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+.chat-input button {
+  background-color: #A33DA5;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+</style>
--- src/components/FriendList.vue
+++ src/components/FriendList.vue
@@ -1,3 +1,21 @@
+<template>
+  <div class="friend-list">
+    <div
+      class="friend-item"
+      v-for="friend in friends"
+      :class="{ 'active': friend.active, 'highlighted': friend.highlighted }"
+      @click="toggleFriendActive(friend); getMessages(friend.id)"
+      @mouseover="friend.highlighted = true"
+      @mouseout="friend.highlighted = false"
+      :key="friend.id"
+    >
+      <img :src="friend.image" alt="Amigos">
+      <h3 class="name">{{ friend.name}}</h3>
+      <h3>{{friend.last_name}}</h3>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
   name: 'FriendList',
@@ -19,7 +37,11 @@ export default {
       if (response.ok) {
         const json_response = await response.json();
         if (json_response) {
-          this.friends = json_response;
+          this.friends = json_response.map(friend => ({
+            ...friend,
+            active: false,
+            highlighted: false,
+          }));
         } else {
           console.error('Error: Friends data is missing');
           console.error(response);
@@ -27,6 +49,15 @@ export default {
       } else {
         console.error('Error calling API:', response.status);
       }
+    },
+    toggleFriendActive(clickedFriend) {
+      this.friends.forEach(friend => {
+        if (friend !== clickedFriend) {
+          friend.active = false;
+          friend.highlighted = false;
+        }
+      });
+      clickedFriend.active = !clickedFriend.active;
     },
     async getMessages(friendId) {
       emitter.emit('getMessages', friendId);
@@ -38,11 +69,39 @@ export default {
 };
 </script>
 
-<template>
-  <div class="friend-list">
-    <div class="friend-item" v-for="friend in friends" @click="getMessages(friend.id)" :key="friend.id">
-      <img :src="friend.image" alt="Amigos">
-      <h3>{{ friend.name + ' ' + friend.last_name }}</h3>
-    </div>
-  </div>
-</template>
+<style scoped>
+.friend-list {
+  width: 300px;
+  background-color: #f0f0f0;
+  display: flex;
+  flex-direction: column;
+  border-right: 3px solid black;
+}
+
+.friend-item {
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  border-bottom: 2px solid black;
+  width: 100%;
+  cursor: pointer;
+}
+
+.friend-item img {
+  width: 50px;
+  height: 50px;
+  border-radius:50%;
+  margin-right: 10px;
+}
+
+.name {
+  padding-right: 5px;
+}
+
+.friend-item:hover,
+.friend-item.active,
+.friend-item.highlighted {
+  background-color: #A33DA5;
+  color: white;
+}
+</style>
